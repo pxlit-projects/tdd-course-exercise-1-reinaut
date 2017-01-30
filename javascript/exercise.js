b0867e07bf5512567eb03fee33d03e8a5d23c03d
Reinaut Krekels <reinaut.krekels@pxl.be>
2017-01-30 14:32:53 +0100

    som met keys-methode

--- javascript/exercise.js
+++ javascript/exercise.js
@@ -1,10 +1,6 @@
 
 function beoordeelSpeculaas(speculaas) {
-	var som = 0;
-	for (var prop in speculaas) if (speculaas.hasOwnProperty(prop)) {
-		if (typeof speculaas[prop] === "number") {
-			som += speculaas[prop];
-		}
-	}
-	return som;
+	return Object.keys(speculaas).reduce(function (previous, currentProperty) {
+		return previous + speculaas[currentProperty];
+	}, 0);
 }
